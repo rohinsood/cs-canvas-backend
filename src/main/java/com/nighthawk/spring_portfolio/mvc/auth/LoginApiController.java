44639af342d69d57d1fdf91d6068594d843546af
vardaansinha <vardsin28@gmail.com>
2023-10-29 21:19:17 -0700

    importing all this nonsense

--- src/main/java/com/nighthawk/spring_portfolio/mvc/auth/LoginApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/auth/LoginApiController.java
@@ -1,5 +1,28 @@
 package com.nighthawk.spring_portfolio.mvc.auth;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestCookieException;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/login")
+
 public class LoginApiController {
     
 }
