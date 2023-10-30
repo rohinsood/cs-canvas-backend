98f9abb155734a034e2448e191ee0270ff1d7c63
vardaansinha <vardsin28@gmail.com>
2023-10-30 02:39:57 -0700

    fixing commits

--- src/main/java/com/nighthawk/spring_portfolio/mvc/ModelInit.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/ModelInit.java
@@ -6,26 +6,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.nighthawk.spring_portfolio.mvc.auth.Person;
-import com.nighthawk.spring_portfolio.mvc.auth.PersonDetailsService;
+
+import com.nighthawk.spring_portfolio.mvc.person.Person;
+import com.nighthawk.spring_portfolio.mvc.person.PersonDetailsService;
 
 import java.util.List;
 
 @Component
 @Configuration // Scans Application for ModelInit Bean, this detects CommandLineRunner
 public class ModelInit {  
+
     @Autowired PersonDetailsService personService;
 
     @Bean
     CommandLineRunner run() {  // The run() method will be executed after the application starts
         return args -> {
-            Person p = new Person();
-            p.setEmail("yippee@y8ipee.com");
-            p.setPasswordHash("password");
 
-            System.out.println(p.toString());
 
-            personService.save(p);
+
+            // Person database is populated with test data
+            Person[] personArray = Person.init();
+            for (Person person : personArray) {
+                //findByNameContainingIgnoreCaseOrEmailContainingIgnoreCase
+                List<Person> personFound = personService.list(person.getName(), person.getEmail());  // lookup
+                if (personFound.size() == 0) {
+                    personService.save(person);  // save
+
+                    // Each "test person" starts with a "test note"
+                }
+            }
+
         };
     }
 }
