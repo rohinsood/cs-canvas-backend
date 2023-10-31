d4000ad09767f24269d60dbc6145522cd75eee18
VishnuAravind12 <vishnuaa77@gmail.com>
2023-10-31 11:28:29 -0700

    controller fo rml

--- src/main/java/com/nighthawk/spring_portfolio/mvc/models/GradePredictionController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/models/GradePredictionController.java
@@ -1,6 +1,5 @@
 package com.nighthawk.spring_portfolio.mvc.models;
 
-import com.nighthawk.spring_portfolio.mvc.models.MultiVarAnalyticsGradeRegression;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
