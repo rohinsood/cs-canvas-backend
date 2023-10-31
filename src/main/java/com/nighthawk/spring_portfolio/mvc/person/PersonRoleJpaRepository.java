5eca8cda1898a1b14a775549eace4c153206e876
rohinsood <rs.rohinsood@gmail.com>
2023-10-30 21:43:31 -0700

    deleting boilerplate

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonRoleJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nighthawk.spring_portfolio.mvc.person;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface  PersonRoleJpaRepository extends JpaRepository<PersonRole, Long> {
-    PersonRole findByName(String name);
-}
