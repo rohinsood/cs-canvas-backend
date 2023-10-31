df16efd93401ff73c8e9eea7caf69f305d8b49f1
VishnuAravind12 <vishnuaa77@gmail.com>
2023-10-30 18:58:07 -0700

    data visualization created

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/models/DataVisualization.java
@@ -0,0 +1,11 @@
+package com.nighthawk.spring_portfolio.mvc.models;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+public class DataVisualization {
+    
+}
