fbb47321aafa35b529414ab07e01b496d002ba02
VishnuAravind12 <vishnuaa77@gmail.com>
2023-11-02 07:13:54 -0700

    minor

--- src/main/java/com/nighthawk/spring_portfolio/mvc/models/GradePredictionController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/models/GradePredictionController.java
@@ -2,7 +2,6 @@ package com.nighthawk.spring_portfolio.mvc.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
