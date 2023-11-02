45eca796aa4959e9b3880cfeed2b554412d62bc7
rohinsood <rs.rohinsood@gmail.com>
2023-11-02 05:44:47 -0700

    update cors config ans ass controller

--- src/main/java/com/nighthawk/spring_portfolio/security/SecurityConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.nighthawk.spring_portfolio.security;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
 /*
 * To enable HTTP Security in Spring, extend the WebSecurityConfigurerAdapter. 
@@ -13,6 +14,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // Provide a default configuration using configure(HttpSecurity http)
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();  // Cross-Site Request Forgery disable for JS Fetch URIs
+        http.csrf(csrf -> csrf.disable()).headers(headers -> headers
+        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
+        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-ExposedHeaders", "*", "Authorization"))
+        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Content-Type", "Authorization", "x-csrf-token"))
+        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-MaxAge", "600"))
+        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST", "GET", "OPTIONS", "HEAD"))
+        //.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "https://nighthawkcoders.github.io", "http://localhost:4000"))
+        );  // Cross-Site Request Forgery disable for JS Fetch URIs
     }
+
 }
