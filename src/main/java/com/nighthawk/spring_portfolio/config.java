2d7d79b86fe29f73fca798add6f639516157d88b
A.PARIKH <akshat1228@gmail.com>
2023-11-05 15:08:25 -0800

    dsadsa

--- src/main/java/com/nighthawk/spring_portfolio/config.java
+++ src/main/java/com/nighthawk/spring_portfolio/config.java
@@ -1,64 +1,69 @@
-package com.nighthawk.spring_portfolio.mvc;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
+package com.nighthawk.spring_portfolio;
 
-import com.nighthawk.spring_portfolio.mvc.auth.Person;
-import com.nighthawk.spring_portfolio.mvc.auth.PersonDetailsService;
+import java.io.FileNotFoundException;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 
-import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
-@Component
-@Configuration // Scans Application for ModelInit Bean, this detects CommandLineRunner
-public class ModelInit {  
-    @Autowired PersonDetailsService personService;
+// @SpringBootApplication annotation is key to building web applications using Java https://spring.io/projects/spring-boot
+@SpringBootApplication
+public class Main {
 
-    @Bean
-    CommandLineRunner run() {  // The run() method will be executed after the application starts
-        return args -> {
-            Person p = new Person();
-            p.setEmail("yippee@y8ipee.com");
-            p.setPasswordHash("password");
-
-            System.out.println(p.toString());
-
-            personService.save(p);
-        };
+    // Starts a spring application as a stand-alone application from the main method
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
     }
-}
+    
+    @EventListener(ApplicationReadyEvent.class)
+    public void applicationStart() {
+        System.out.println("Application started!");
+        System.out.println("Admin Key: " + System.getenv("ADMIN_KEY"));
 
-package com.nighthawk.spring_portfolio.mvc;
+        Resource coolDbResource = new FileSystemResource("volumes/sqlite.db");
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.nighthawk.spring_portfolio.mvc.auth.Person;
-import com.nighthawk.spring_portfolio.mvc.auth.PersonDetailsService;
-
-import java.util.List;
-
-@Component
-@Configuration // Scans Application for ModelInit Bean, this detects CommandLineRunner
-public class ModelInit {  
-    @Autowired PersonDetailsService personService;
-
-    @Bean
-    CommandLineRunner run() {  // The run() method will be executed after the application starts
-        return args -> {
-            Person p = new Person();
-            p.setEmail("yippee@y8ipee.com");
-            p.setPasswordHash("password");
-
-            System.out.println(p.toString());
-
-            personService.save(p);
-        };
+        // Check if the file exists
+        if (coolDbResource.exists()) {
+            System.out.println("sqlite.db file found");
+        }
     }
-}
 
+}
+package com.nighthawk.spring_portfolio;
+
+import java.io.FileNotFoundException;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+// @SpringBootApplication annotation is key to building web applications using Java https://spring.io/projects/spring-boot
+@SpringBootApplication
+public class Main {
+
+    // Starts a spring application as a stand-alone application from the main method
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+    
+    @EventListener(ApplicationReadyEvent.class)
+    public void applicationStart() {
+        System.out.println("Application started!");
+        System.out.println("Admin Key: " + System.getenv("ADMIN_KEY"));
+
+        Resource coolDbResource = new FileSystemResource("volumes/sqlite.db");
+
+        // Check if the file exists
+        if (coolDbResource.exists()) {
+            System.out.println("sqlite.db file found");
+        }
+    }
+
+}
